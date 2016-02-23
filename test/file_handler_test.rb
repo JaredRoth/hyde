9e02019d469824206b0bee7c1afce86cf750a436
Jared Roth <jared.roth@gmail.com>
2016-02-22 18:36:14 -0700

    Passing tests

--- test/file_handler_test.rb
+++ test/file_handler_test.rb
@@ -6,18 +6,36 @@ class FileHandlerTest < Minitest::Test
 
   def test_it_navigates_to_the_input_directory
     fh = FileHandler.new
-    fh.move('/bin')
-    assert_equal "/bin" ,FileUtils.pwd
+    fh.move('../lib')
+    assert_equal "lib", File.basename(Dir.pwd)
+    FileUtils.cd('../test')
   end
 
   def test_it_creates_an_empty_directory
     fh = FileHandler.new
-    # fh.move('/test-dir')
-    fh.create('/test-dir')
-    fh.move('/test-dir/source/posts')
-    assert_equal '/posts', FileUtils.pwd
+    fh.create('test-dir')
+
+    fh.move('test-dir/output')
+    assert_equal 'output', File.basename(Dir.pwd)
+    FileUtils.cd('../..')
+
+    fh.move('test-dir/source')
+    assert_equal 'source', File.basename(Dir.pwd)
+    FileUtils.cd('../..')
+
+    fh.move('test-dir/source/css')
+    assert_equal 'css', File.basename(Dir.pwd)
+    FileUtils.cd('../../..')
+
+    fh.move('test-dir/source/pages')
+    assert_equal 'pages', File.basename(Dir.pwd)
+    FileUtils.cd('../../..')
+
+    fh.move('test-dir/source/posts')
+    assert_equal 'posts', File.basename(Dir.pwd)
+    FileUtils.cd('../../..')
   end
-  
+
   #create an empty directory in given directory
   #create, inside the new directory, an empty directory called output
   # and a directory called source.
