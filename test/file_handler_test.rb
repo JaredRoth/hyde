3b2ea5c41a34941c2e75dfbcffe127dba2340baa
Jeneve Parrish <jeneve.parrish@gmail.com>
2016-02-22 21:11:53 -0700

    touch method with passing test

--- test/file_handler_test.rb
+++ test/file_handler_test.rb
@@ -36,11 +36,15 @@ class FileHandlerTest < Minitest::Test
     FileUtils.cd('../../..')
   end
 
-  #create an empty directory in given directory
-  #create, inside the new directory, an empty directory called output
-  # and a directory called source.
-  # inside the source directory, create dir, css, pages, post and the file
-  #index.markdown
+  def test_it_makes_a_new_empty_file_in_a_directory
+    fh = FileHandler.new
+    fh.create('test-dir')
 
+    fh.move('test-dir/source/css')
+    main = fh.touch('main.css')
+    assert File.exist?(main)
+    assert_equal "", File.read('main.css')
 
+    FileUtils.cd('../../..')
+  end
 end
