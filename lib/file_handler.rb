b5ebc57b3e3cb074df9baadea6fe98f957226d18
Jared Roth <jared.roth@gmail.com>
2016-02-26 08:35:16 -0700

    Final cleanup

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -5,7 +5,6 @@ require 'listen'
 require 'pry'
 
 class FileHandler
-  attr_reader :directory
 
   def initialize(directory)
     @directory = directory
@@ -16,19 +15,7 @@ class FileHandler
   end
 
   def inject(html)
-    ERB.new(File.read("#{directory}/source/layouts/default.html.erb")).result(binding)
+    ERB.new(File.read("#{@directory}/source/layouts/default.html.erb")).result(binding)
   end
+  
 end
-
-# :nocov:
-if __FILE__ == $0
-  dir = "test-dir"
-  fh = FileHandler.new(dir)
-  puts fh.directory + '/source'
-  fh.create_tree
-  fh.populate_tree
-  fh.create_post("My Post")
-  fh.copy_source
-  FileUtils.remove_dir('test-dir', force = true)
-end
-# :nocov:
